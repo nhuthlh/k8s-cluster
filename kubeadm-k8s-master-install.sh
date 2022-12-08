b02f1563d8092c0a36383b45f17d9800276632e1
Nhut Huynh <67490369+nhuthlh@users.noreply.github.com>
2022-12-08 16:29:20 +0700

    Update kubeadm-k8s-master-install.sh
    update 1

--- kubeadm-k8s-master-install.sh
+++ kubeadm-k8s-master-install.sh
@@ -1,75 +1,74 @@
 #!/bin/bash
-
-UBUNTU_VERSION=16.04
-K8S_VERSION=1.11.3-00
-node_type=master
-
-echo "Ubuntu version: ${UBUNTU_VERSION}"
-echo "K8s version: ${K8S_VERSION}"
-echo "K8s node type: ${node_type}"
-echo
-#Update all installed packages.
-sudo apt-get update
-sudo apt-get upgrade
-
-#if you get an error similar to
-#'[ERROR Swap]: running with swap on is not supported. Please disable swap', disable swap:
+echo "Disabling swap...."
 sudo swapoff -a
+sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-# install some utils
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+echo "Installing necessary dependencies...."
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
-#Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Setting up hostname...."
+sudo hostnamectl set-hostname "k8s-master"
+IP_ADDRESS=`hostname -I|cut -d" " -f 1`
+sudo echo "${IP_ADDRESS}  k8s-master" >> /etc/hosts
 
-if [ $UBUNTU_VERSION == "16.04" ]; then
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-elif [ $UBUNTU_VERSION == "18.04" ]; then
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-else
-    #default tested version
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-fi
-sudo apt-get update
-sudo apt-get install -y docker.io
+#Enable some kernel modules
+cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF		
 
-#Install NFS client
-sudo apt-get install -y nfs-common
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-#Enable docker service
-sudo systemctl enable docker.service
+#Setting networking for K8s
+cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward                = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
 
-#Update the apt source list
+sudo sysctl --system
+
+#Install containerd
+sudo apt-get update && sudo apt-get install -y containerd
+		
+#Make a folder to keep configuration files
+sudo mkdir -p /etc/containerd
+			
+#Generate the default config file
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+#Restart the containerd to ensure the new config file is used.
+sudo systemctl restart containerd
+			
+#Check the fstab file to confirm swap is disabled
+sudo cat /etc/fstab
+
+#Install K8s
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://apt.kubernetes.io/ kubernetes-xenial main"
 
-#Install K8s components
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io kubernetes-xenial main
+EOF
+
 sudo apt-get update
-sudo apt-get install -y kubelet=$K8S_VERSION kubeadm=$K8S_VERSION kubectl=$K8S_VERSION
 
+sudo apt-get install -y kubelet=1.25.0-00 kubeadm=1.25.0-00 kubectl=1.25.0-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
-#Initialize the k8s cluster
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+#Initialize cluster from the control node
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.25.0
 
-sleep 60
+#Setting up networking by installing Calico networking and network policy
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-#Create .kube file if it does not exists
-mkdir -p $HOME/.kube
+#Join worker nodes to the cluster
+kubeadm token create --print-join-command
 
-#Move Kubernetes config file if it exists
-if [ -f $HOME/.kube/config ]; then
-    mv $HOME/.kube/config $HOME/.kube/config.back
-fi
-
-sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#if you are using a single node which acts as both a master and a worker
-#untaint the node so that pods will get scheduled:
-kubectl taint nodes --all node-role.kubernetes.io/master-
-
-#Install Flannel network
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
-
-echo "Done."
+echo "Testing Kubernetes namespaces... "
+kubectl get pods --all-namespaces
+echo "Testing Kubernetes nodes... "
+kubectl get nodes
+echo "All ok ;)"
