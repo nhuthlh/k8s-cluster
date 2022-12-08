3e1b5b950d878f04e24db138d5a98303da7d9f56
Nhut Huynh <67490369+nhuthlh@users.noreply.github.com>
2022-12-08 17:31:26 +0700

    Update kubeadm-k8s-worker1-install.sh

--- kubeadm-k8s-worker1-install.sh
+++ kubeadm-k8s-worker1-install.sh
@@ -8,22 +8,22 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 
 echo "Setting up hostname...."
 sudo hostnamectl set-hostname "k8s-worker1"
-IP_ADDRESS=`hostname -I|cut -d" " -f 1`
-sudo echo "${IP_ADDRESS}  k8s-worker1" >> /etc/hosts
+IP_ADDRESS=$(hostname -I|cut -d" " -f 1)
+sudo echo "${IP_ADDRESS}  k8s-worker1" | sudo tee -a /etc/hosts
 
 #Enable some kernel modules
 cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
-EOF		
+EOF
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
 #Setting networking for K8s
-cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward                = 1
+net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
@@ -49,7 +49,7 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io kubernetes-xenial main
 EOF
 
